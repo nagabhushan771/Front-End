1a7de014bc6f09f97a2aebf9d38914c8d63e5a27
nagabhushan771 <naag000001@gmail.com>
2022-06-04 21:55:52 +0530

    js practice

---
+++ js/objectsAndDate.js
@@ -0,0 +1,21 @@
+// var c = {
+//     Name : 'Nagabhushan',
+//     Year : '2020',
+//     current : 'None'
+// }
+
+// console.log(c.Name);
+// console.log(c.current);
+
+// var d = new Date();
+// console.log(d);
+// console.log(d.getDate());
+// console.log(d.getFullYear());
+// console.log(d.getMilliseconds());
+// console.log(d.getMonth());
+// console.log(d.getDay());
+// console.log(d.getHours());
+
+for(var i = 21; i>=1; i-=2){
+    console.log(i);
+}
